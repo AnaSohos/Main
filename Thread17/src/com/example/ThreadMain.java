fa8d4bbb661a08e39d4e017681ff7c249a3d8f47
AnaSohos <ana.soho@yandex.ru>
2020-10-30 15:00:43 +0300

    Задачка работает

---
+++ Thread17/src/com/example/ThreadMain.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class ThreadMain {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        DetalsRan detal = new DetalsRan("1");
+        DetalsRan detal2 = new DetalsRan("2");
+        DetalsRan detal3 = new DetalsRan("3");
+        DetalsRan detal4 = new DetalsRan("4");
+        DetalsRan detal5 = new DetalsRan("5");
+        DetalsRan detal6 = new DetalsRan("6");
+        DetalsRan detal7 = new DetalsRan("7");
+        DetalsRan detal8 = new DetalsRan("8");
+        DetalsRan detal9 = new DetalsRan("9");
+        DetalsRan detal10 = new DetalsRan("10");
+        DetalsRan detal11 = new DetalsRan("11");
+        DetalsRan detal12 = new DetalsRan("12");
+        DetalsRan detal13 = new DetalsRan("13");
+        DetalsRan detal14 = new DetalsRan("14");
+        DetalsRan detal15 = new DetalsRan("15");
+        DetalsRan detal16 = new DetalsRan("16");
+        DetalsRan detal17 = new DetalsRan("17");
+        DetalsRan detal18 = new DetalsRan("18");
+        DetalsRan detal19 = new DetalsRan("19");
+        DetalsRan detal20 = new DetalsRan("20");
+
+
+    }
+}
